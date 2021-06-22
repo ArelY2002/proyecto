b1367a4f1bf0f21abac3be65450ccd8cb61ed589
JennyMP5 <86113827+JennyMP5@users.noreply.github.com>
2021-06-21 22:26:19 -0500

    Create trabajo grupal.cpp
    Buenas noches 
    Antes todo un cordial saludo de mi parte soy Jenniffer Mina. Mi parte del trabajo como primer integrante fue: “Mostrar por pantalla los elementos de la matriz” para hacer este proceso utilice la función “do while”   
    …………
    Pero antes de darle la explicación de “do while” le voy a decir en breve porque ocupe “cout<<”
    Como podemos ver cuando comenzamos el programa nos muestra la galería iostream la cual sirve para usar la función de cin y cout, cin es de entrada de datos y cout es de salida de datos entonces cout lo utilice para que muestre el siguiente mensaje “la matriz1 es” y “la matriz2 es” para saber cual de las dos maestrías estoy observando también lo utilizamos para mostrar los resultados de cada uno de los procesos matemáticos que hicimos. 
    ………………
    Do while 
    Por así decirlo esta estructura do while primero actúa y después piensa es decir primer hay que tener en cuenta que el do while hace un conjunto de instrucciones y lego va a comprobar si se cumple la condición y vuelve a ejecutarse hasta que deje de cumplirse.
    Lo que estamos haciendo dentro de las líneas de código de d while es:
    Mostrar por pantalla la matriz1[c] con la variable acumulador “[c]”  
    Luego ponemos a funcionar +1 en código quedaría (c=c+1) o su abreviación (c +=1) este va a comenzar a contar/acumular hasta el límite.
    ………………
    if 
    es una condición, la cual hace un proceso por el lado verdadero y por el falso finaliza, entonces el proceso que hace “if” es un limitante, es decir, hasta donde quiero que llegue el contador y en su codificación quedaría “if(c<10)”.
    Todo este proceso se repite para mostrar la matriz2
    Gracias por su atención :)

--- trabajo grupal.cpp
+++ trabajo grupal.cpp
@@ -6,26 +6,32 @@ int c=0;
 float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//presentar primera matriz 
+ //jenniffer Katherine Mina Preciado
+cout<<"La matriz1 es: "<<endl;
+ do{
+ cout<<matriz1[c];
+ c=c+1;
+ if(c<10){
+    cout<<" - ";
+ }
+     
+ }while(c<10);
+ cout<<endl;
+ c=0;
+ 
+ cout<<"la matriz2 es: "<<endl;
+ do{
+ cout<<matriz2[c];
+ c=c+1;
+ if(c<10){
+    cout<<"  - ";
+ }
+     
+ }while(c<10);
+ cout<<endl;
+ c=0;
+//----------------------------------
 //Rosa Antonella Posso Becerra
 //Suma de la matriz1 y matriz2 
 
@@ -41,19 +47,7 @@ if(c<10){
 cout<<endl;
 c=0;
 //Zam
-//Andrea Palacios Becerra
-//división de la matriz1 para la matriz2
- 
- cout<<"la matriz division es:"<<endl<<"division[10]=";
-do{ 
-cout<<matriz1[c]/matriz2[c];
-c=c+1;
-if(c<10){
-   cout<<"  - ";
-}
-   
-}while(c<10); 
-cout<<endl;
-c=0;
-   return 0;
+
+return 0;
+
 }
